0095131e587ba94efaa52cc33b07d0f01bc75472
Joel De Anda <166152379+Joelitoox@users.noreply.github.com>
2025-05-30 17:48:12 -0600

    Add files via upload

---
+++ WcfService2/WcfService2/Service1.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WcfService2
+{
+    public class Service1 : IService1
+    {
+        public double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Dividir(double a, double b)
+        {
+            if (b == 0)
+                throw new FaultException("No se puede dividir entre cero.");
+            return a / b;
+        }
+
+        public double Potencia(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Modulo(double a, double b)
+        {
+            if (b == 0)
+                throw new FaultException("No se puede calcular el módulo con divisor cero.");
+            return a % b;
+        }
+    }
+}
